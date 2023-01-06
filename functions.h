0574d693eea1afd91b401a0bd03d5bff570e437a
mammadnet <moohamadiann@gmail.com>
2023-01-06 14:28:10 -0800

    add function to manage the new professors and courses

--- functions.h
+++ functions.h
@@ -5,11 +5,14 @@
 #define CYNC  "\x1B[36m"        // Cyan color
 
 int stdNum=0;                   // Number of students
+int profNum=0;                  // Number of professors
+int corsNum=0;                  // Number of courses
 stdNode *stdHead=NULL;          // First element of student link list
+profNode *profHead=NULL;        // First element of professor link list
+courseNode *corsHead=NULL;      // First element of course link list
 
 
-
-stdNode *addStudent(){         // Add new student to end of the linklist
+void addStudent(){         // Add new student to end of the linklist
     stdNode *p, *newStd;
     newStd=getStdNode();
     if(stdHead == NULL){           // Checking that the first element of the linklist is filled
@@ -36,20 +39,98 @@ stdNode *addStudent(){         // Add new student to end of the linklist
         printf("Enter the national code:\n");
         scanf("%s", ntlTmp);
         if(checkNtnlNum(ntlTmp)) break;
-        printf(REDC "->Enter the correct national code(10 digit)<-\n" NRMC);
+        printf(REDC "ERR->Enter the correct national code(10 digit)\n" NRMC);
     }
-    printf("here2");
     strcpy(newStd->nationalCode, ntlTmp);
-    newStd->stdCode = stdCodeCrtr(stdNum);
     stdNum++;
+    newStd->stdCode = stdCodeCrtr();
     system("cls");
     printf(GRNC "**New student was added**\n" NRMC);
-    return stdHead;
+}
+//-----------------------------------------------------------
+
+void addProf(){                         // Add new professor to end of the link list
+    profNode *p, *newProf;
+    newProf = getProfNode();
+    if(profHead == NULL){               // Checking that the first element of the linklist is filled
+        profHead = newProf;
+    }else{                              // Add a new node to the end of the linked list
+        p = profHead;
+        while(p->next != NULL){
+            p = p->next;
+        }
+        p->next = newProf;
+    }
+
+    system("cls");
+    printf(CYNC "**Add new professor**\n" NRMC);    
+    printf("Enter the first name:\n");
+    scanf("%s", newProf->name);
+    printf("Enter last name:\n");
+    scanf("%s", newProf->lastName);
+    printf("Enter national code:\n");
+    char ntlTmp[12];
+    while(1)
+    {
+        printf("Enter the national code:\n");
+        scanf("%s", ntlTmp);
+        if(checkNtnlNum(ntlTmp)) break;
+        printf(REDC "ERR->Enter the correct national code(10 digit)\n" NRMC);
+    }
+    strcpy(newProf->nationalCode, ntlTmp);
+    profNum++;                                  
+    newProf->code = profCodeCrtr();
+    system("cls");
+    printf(GRNC "**New professor was added**\n" NRMC);
+}
+//------------------------------------------------------------------
+void addCourse(){
+    courseNode *p, *newCors;
+    newCors = getCourseNode();
+    if(corsHead == NULL){
+        corsHead = newCors;
+    }else{
+        p = corsHead;
+        while(p->next != NULL){
+            p = p->next;
+        }
+        p->next = newCors;
+    }
+    system("cls");
+    printf(CYNC "**Add new course**\n" NRMC);
+    printf("Enter name of the course:\n");
+    scanf("%s", newCors->name);
+    int un;
+    while (1)
+    {
+        printf("Enter number of unit course:\n");
+        scanf("%d", &un);
+        if(un <= 3 && un>=1) break;
+        printf(REDC "ERR->The number of units must be between 1 and 3.\n" NRMC);
+    }
+    newCors->unit = un;
+    corsNum++;
+    newCors->code=corsCodeCrtr();
+    system("cls");
+    printf(GRNC "**new course was added**\n" NRMC);
+}
+//---------------------------------------------------------------------------
+
+int stdCodeCrtr(){                           // Creat a student code based on the current year 
+    int code = curntYear()*10000+1600;
+    code += stdNum;
+    return code;
 }
 
-int stdCodeCrtr(int stdNum){            // Creating a student code based on the current year 
-    int code = curntYear()*10000+1600;
-    code += stdNum+1;
+int profCodeCrtr(){                          // Creat a professor code based on entry year
+    int code=curntYear()*10000 + 1100;
+    code += profNum;
+    return code;
+}
+
+int corsCodeCrtr(){                         // Creat a course code
+    int code = curntYear()*10000 + 7700;
+    code += corsNum;
     return code;
 }
 
