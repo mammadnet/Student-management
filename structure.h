e8d344853283d7487e4b1750f22b8af28aba06eb
mammadnet <moohamadiann@gmail.com>
2023-01-08 13:24:52 -0800

    remove extra structures

--- structure.h
+++ structure.h
@@ -6,35 +6,27 @@ typedef struct courses
     struct courses *next;       // Point to next course (link list structure)
 } courseNode;
 
-typedef struct studentCourse        //-------
-{
-    courseNode *course;
-    struct studentCourse *next;       // Point to the next course of the student
-} stdCourseNode;
+
 
 typedef struct student{
     char name[20];
     char lastName[20];
     int year;
     int stdCode;
-    char nationalCode[12];
-    stdCourseNode *course;
-    struct student *next;       //Point to next student (link list structure)
+    char idCode[12];
+    int crsCode[10];
+    int courseNum;
+    int units;
+    struct student *next;       // Point to next student (link list structure)
 }stdNode;
-
-typedef struct professorCourse        //-------
-{
-    courseNode *course;
-    struct professorCourse *next;       // Point to the next course of the professor
-} profCourseNode;
 
 typedef struct professor{
     char name[20];
     char lastName[20];
-    char nationalCode[12];
+    char idCode[12];
     int code;
     int courseNum;
-    profCourseNode *course;
+    int crsCode[5];
     struct professor *next;             // Point to next professor (link list structure)
 }profNode;
 
