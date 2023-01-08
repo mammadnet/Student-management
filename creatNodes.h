ce19dbd44cd4c87f807c4f09516bdbbda5cd7e85
mammadnet <moohamadiann@gmail.com>
2023-01-08 13:23:07 -0800

    remove some elements in structurs

--- creatNodes.h
+++ creatNodes.h
@@ -5,34 +5,17 @@ courseNode *getCourseNode(){      // For creat a course node and return a pointe
     return tmp;
 }
 
-stdCourseNode *getStdCourseNode(){
-    stdCourseNode *tmp;
-    tmp = (stdCourseNode *)malloc(sizeof(stdCourseNode));
-    tmp->course=NULL;
-    tmp->next=NULL;
-    return tmp;
-}
-
 stdNode *getStdNode(){
     stdNode *tmp;
     tmp = (stdNode *)malloc(sizeof(stdNode));
-    tmp->course=NULL;
     tmp->next=NULL;
     return tmp;
 }
 
-profCourseNode *getProfCourseNode(){
-    profCourseNode *tmp;
-    tmp=(profCourseNode *)malloc(sizeof(profCourseNode));
-    tmp->course=NULL;
-    tmp->next=NULL;
-    return tmp;
-}
 
 profNode *getProfNode(){
     profNode *tmp;
     tmp=(profNode *)malloc(sizeof(profNode));
-    tmp->course=NULL;
     tmp->next=NULL;
     return tmp;
 }
